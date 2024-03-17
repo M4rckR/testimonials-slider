5ef5b5109d40a415e1f4299792a7caefd1a5928c
Marcos Romero <marcromerogar4@gmail.com>
2024-03-17 18:03:47 -0500

    Preludio de la pagina hasta ahora

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./*.html", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "m-dark-blue" : "hsl(240, 38%, 20%)",
+        "m-grayish-blue": "hsl(240, 18%, 77%)"
+      },
+      fontFamily: {
+        // 300, 500, 700
+        // ligth, medium y bold
+        "m-inter" : "['Inter', sans-serif]"
+      },
+    },
   },
   plugins: [],
 }
